fca3a38f81d7946fd9fb60cc2a01fba5d19b286d
Thomas de Jong <thomas@tdejong.io>
2022-03-25 10:07:41 +0100

    Installed django-environ and changed & removed secret key from the code

--- githubmotivator/settings.py
+++ githubmotivator/settings.py
@@ -11,16 +11,21 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# reading .env file
+env = environ.Env()
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xpqs5dk_1n1rvz@-ow_i_vl6drlapiquptdrr+70#*me!&k$*$'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
