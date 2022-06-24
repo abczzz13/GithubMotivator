3924291d0007e4866be00bdad398607e950c3666
Thomas de Jong <thomas@tdejong.io>
2022-06-24 11:14:49 +0200

    Integrated the Mollie API monkeypatch calls as pytest fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,158 @@
 """Fixtures for Pytest"""
+from typing import Any
+
 import pytest
+from django.conf import settings
 from django.utils import timezone
 from mixer.backend.django import mixer
+from mollie.api.error import IdentifierError
+from mollie.api.resources.base import ResourceBase
+from mollie.api.resources.payment_refunds import PaymentRefunds
+from mollie.api.resources.payments import Payments
 
 from motivator.models import Goal
+from motivator.payments import MolliePaymentProvider
 from users.models import User, UserMotivator
+
+
+class MockPaymentProvider:
+    def patched_create(self, payload: dict[str, Any]) -> dict[str, Any]:
+        """Patched function to mimic the external Mollie API call"""
+        payment = {
+            "resource": "payment",
+            "id": "tr_QdCtWBhJAD",
+            "mode": "test",
+            "createdAt": "2022-06-14T13:51:24+00:00",
+            "amount": {"value": "10.00", "currency": "EUR"},
+            "description": "Test #123",
+            "method": "ideal",
+            "metadata": {"order_id": "12345"},
+            "status": "open",
+            "isCancelable": False,
+            "expiresAt": "2022-06-14T14:06:24+00:00",
+            "profileId": "pfl_3uCm3wEJgB",
+            "sequenceType": "oneoff",
+            "redirectUrl": "https://webshop.example.org/order/12345/",
+            "webhookUrl": "https://64eb-86-83-204-47.eu.ngrok.io",
+            "_links": {
+                "self": {
+                    "href": "https://api.mollie.com/v2/payments/tr_QdCtWBhJAD",
+                    "type": "application/hal+json",
+                },
+                "checkout": {
+                    "href": "https://www.mollie.com/checkout/select-issuer/ideal/QdCtWBhJAD",
+                    "type": "text/html",
+                },
+                "dashboard": {
+                    "href": "https://www.mollie.com/dashboard/org_1251741/payments/tr_QdCtWBhJAD",
+                    "type": "text/html",
+                },
+                "documentation": {
+                    "href": "https://docs.mollie.com/reference/v2/payments-api/create-payment",
+                    "type": "text/html",
+                },
+            },
+        }
+        payment["amount"]["value"] = payload["amount"]["value"]
+        payment["description"] = payload["description"]
+        payment["metadata"] = payload["metadata"]
+        payment["redirectUrl"] = payload["redirectUrl"]
+        payment["webhookUrl"] = payload["webhookUrl"]
+        return payment
+
+    def patched_get(self, id: str) -> dict[str, Any]:
+        """Patched function to mimic the external Mollie API call"""
+        payment = {
+            "resource": "payment",
+            "id": "tr_QdCtWBhJAD",
+            "mode": "test",
+            "createdAt": "2022-06-14T13:51:24+00:00",
+            "amount": {"value": "10.00", "currency": "EUR"},
+            "description": "Test #123",
+            "method": "ideal",
+            "metadata": {"order_id": "12345"},
+            "status": "failed",
+            "isCancelable": False,
+            "expiresAt": "2022-06-14T14:06:24+00:00",
+            "profileId": settings.MOLLIE_PROFILE_ID,
+            "sequenceType": "oneoff",
+            "redirectUrl": "https://webshop.example.org/order/12345/",
+            "webhookUrl": "https://64eb-86-83-204-47.eu.ngrok.io",
+            "_links": {
+                "self": {
+                    "href": "https://api.mollie.com/v2/payments/tr_QdCtWBhJAD",
+                    "type": "application/hal+json",
+                },
+                "checkout": {
+                    "href": "https://www.mollie.com/checkout/select-issuer/ideal/QdCtWBhJAD",
+                    "type": "text/html",
+                },
+                "dashboard": {
+                    "href": "https://www.mollie.com/dashboard/org_1251741/payments/tr_QdCtWBhJAD",
+                    "type": "text/html",
+                },
+                "documentation": {
+                    "href": "https://docs.mollie.com/reference/v2/payments-api/create-payment",
+                    "type": "text/html",
+                },
+            },
+        }
+        if id != payment["id"]:
+            raise IdentifierError("Payment could not be found")
+        return payment
+
+    def patched_refund(self, payload: dict[str, Any]) -> dict[str, Any]:
+        refund = {
+            "resource": "refund",
+            "id": "re_4qqhO89gsT",
+            "amount": {"currency": "EUR", "value": "5.95"},
+            "status": "pending",
+            "createdAt": "2018-03-14T17:09:02.0Z",
+            "description": "Order #33",
+            "metadata": {"bookkeeping_id": 12345},
+            "paymentId": "tr_QdCtWBhJAD",
+            "_links": {
+                "self": {
+                    "href": "https://api.mollie.com/v2/payments/tr_WDqYK6vllg/refunds/re_4qqhO89gsT",
+                    "type": "application/hal+json",
+                },
+                "payment": {
+                    "href": "https://api.mollie.com/v2/payments/tr_WDqYK6vllg",
+                    "type": "application/hal+json",
+                },
+                "documentation": {
+                    "href": "https://docs.mollie.com/reference/v2/refunds-api/create-payment-refund",
+                    "type": "text/html",
+                },
+            },
+        }
+        refund["amount"]["value"] = payload["amount"]["value"]
+        refund["description"] = payload["description"]
+        refund["metadata"] = payload["metadata"]
+        return refund
+
+    def patched_on(self, payment: dict[str, Any]) -> str:
+        return self.with_parent_id(payment["id"])
+
+
+@pytest.fixture()
+def patched_create(monkeypatch: pytest.MonkeyPatch) -> None:
+    monkeypatch.setattr(ResourceBase, "create", MockPaymentProvider.patched_create)
+
+
+@pytest.fixture()
+def patched_get(monkeypatch: pytest.MonkeyPatch) -> None:
+    monkeypatch.setattr(Payments, "get", MockPaymentProvider.patched_get)
+
+
+@pytest.fixture()
+def patched_on(monkeypatch: pytest.MonkeyPatch) -> None:
+    monkeypatch.setattr(PaymentRefunds, "on", MockPaymentProvider.patched_on)
+
+
+@pytest.fixture()
+def patched_refund(monkeypatch: pytest.MonkeyPatch) -> None:
+    monkeypatch.setattr(ResourceBase, "create", MockPaymentProvider.patched_refund)
 
 
 @pytest.fixture()
@@ -25,6 +173,14 @@ def goal(user):
     motivator_goal = mixer.blend(Goal, start_date=start_date, end_date=end_date, amount=amount)
     motivator_goal.user = user.user
     return motivator_goal
+
+
+@pytest.fixture()
+def payment(monkeypatch: pytest.MonkeyPatch, goal):
+    monkeypatch.setattr(ResourceBase, "create", MockPaymentProvider.patched_create)
+    client = MolliePaymentProvider()
+    payment = client.create_payment(goal)
+    return payment
 
 
 @pytest.fixture()
--- tests/motivator/unit/test_motivator_views.py
+++ tests/motivator/unit/test_motivator_views.py
@@ -1,6 +1,16 @@
 """Testing the view functions of the Motivator App"""
+import pytest
 from django.urls import reverse
-from motivator.views import CreateGoal, DetailGoal, ListGoal, index
+from mollie.api.resources.payments import Payments
+from motivator.models import Payment
+from motivator.views import (
+    CreateGoal,
+    DetailGoal,
+    ListGoal,
+    get_payment_link,
+    index,
+    mollie_webhook,
+)
 
 
 def test_motivator_index(rf):
@@ -60,3 +70,77 @@ def test_motivator_goals_create(rf, user):
     response = CreateGoal.as_view()(request)
 
     assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_motivator_get_payment_link(rf, goal):
+    """
+    GIVEN
+    WHEN
+    THEN
+    """
+    path = reverse("get-payment-link", args=[goal.pk])
+    request = rf.get(path)
+
+    response = get_payment_link(request, pk=goal.pk)
+
+    payment = Payment.objects.filter(goal=goal.pk).first()
+
+    assert response.status_code == 302
+    assert response.url == payment.checkout_url
+
+
+@pytest.mark.django_db
+def test_motivator_get_payment_link_invalid(rf):
+    """
+    GIVEN
+    WHEN
+    THEN
+    """
+    goal_id = 99
+    path = reverse("get-payment-link", args=[goal_id])
+    request = rf.get(path)
+
+    response = get_payment_link(request, pk=goal_id)
+
+    assert response.status_code == 302
+    assert response.url == "/goals/"
+
+
+@pytest.mark.django_db
+def test_motivator_webhook_valid(patched_get, rf, payment):
+    """
+    GIVEN
+    WHEN
+    THEN
+    """
+    # monkeypatch.setattr(Payments, "get", MockPaymentProvider.patched_get)
+
+    path = reverse("mollie-webhook")
+    request = rf.post(path, data={"id": payment.mollie_id})
+
+    response = mollie_webhook(request)
+
+    updated_payment = Payment.objects.filter(mollie_id=payment.mollie_id).first()
+
+    assert response.status_code == 200
+    assert updated_payment.payment_status == "f"
+
+
+@pytest.mark.parametrize(
+    "data",
+    [{}, {"id": "test007"}, {"else": "something"}],
+)
+@pytest.mark.django_db
+def test_motivator_webhook_invalid(rf, data):
+    """
+    GIVEN
+    WHEN
+    THEN
+    """
+    path = reverse("mollie-webhook")
+    request = rf.post(path, data=data)
+
+    response = mollie_webhook(request)
+
+    assert response.status_code == 400
